e5b9d252b33f24dec015700bee273cd22adcf166
Stouse49 <Stouse49@gmail.com>
2017-11-04 21:02:00 -0700

    Add check for 51% activation for updating block times while mining

--- src/miner.cpp
+++ src/miner.cpp
@@ -64,7 +64,10 @@ public:
 int64_t UpdateTime(CBlockHeader* pblock, const Consensus::Params& consensusParams, const CBlockIndex* pindexPrev)
 {
     int64_t nOldTime = pblock->nTime;
-    int64_t nNewTime = std::max(pindexPrev->GetMinTimeNext(), GetAdjustedTime());
+    int64_t nNewTime = pindexPrev->nHeight+1 > consensusParams.octoberFork ?
+                std::max(pindexPrev->GetMinTimeNext(), GetAdjustedTime()) :
+                std::max(pindexPrev->GetMedianTimePast()+1, GetAdjustedTime());
+
 
     if (nOldTime < nNewTime)
         pblock->nTime = nNewTime;
