c5924fe152bdf1ea46d399a7c6351bc159bfec2e
Stouse49 <Stouse49@gmail.com>
2017-11-07 20:01:31 -0800

    RPC Tests (#3):  Fix tests that failed due to incorrect coin values (10000 replaced 50)

--- qa/rpc-tests/walletbackup.py
+++ qa/rpc-tests/walletbackup.py
@@ -112,9 +112,9 @@ class WalletBackupTest(BitcoinTestFramework):
         self.nodes[3].generate(100)
         sync_blocks(self.nodes)
 
-        assert_equal(self.nodes[0].getbalance(), 50)
-        assert_equal(self.nodes[1].getbalance(), 50)
-        assert_equal(self.nodes[2].getbalance(), 50)
+        assert_equal(self.nodes[0].getbalance(), 10000)
+        assert_equal(self.nodes[1].getbalance(), 10000)
+        assert_equal(self.nodes[2].getbalance(), 10000)
         assert_equal(self.nodes[3].getbalance(), 0)
 
         logging.info("Creating transactions")
@@ -146,8 +146,8 @@ class WalletBackupTest(BitcoinTestFramework):
         total = balance0 + balance1 + balance2 + balance3
 
         # At this point, there are 214 blocks (103 for setup, then 10 rounds, then 101.)
-        # 114 are mature, so the sum of all wallets should be 114 * 50 = 5700.
-        assert_equal(total, 5700)
+        # 114 are mature, so the sum of all wallets should be 114 * 10000 = 1140000.
+        assert_equal(total, 1140000)
 
         ##
         # Test restoring spender wallets from backups
