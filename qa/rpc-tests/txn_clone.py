c05807703e145cf80b3074c317dd6869b2a164c8
Stouse49 <Stouse49@gmail.com>
2017-12-10 18:01:48 -0800

    RPC Tests(#3):  Fix 4 other tests by updating amounts based on block rewards and default tx fees

--- qa/rpc-tests/txn_clone.py
+++ qa/rpc-tests/txn_clone.py
@@ -27,7 +27,7 @@ class TxnMallTest(BitcoinTestFramework):
 
     def run_test(self):
         # All nodes should start with 1,250 BTC:
-        starting_balance = 1250
+        starting_balance = 250000
         for i in range(4):
             assert_equal(self.nodes[i].getbalance(), starting_balance)
             self.nodes[i].getnewaddress("")  # bug workaround, coins generated assigned to first getnewaddress!
@@ -132,7 +132,7 @@ class TxnMallTest(BitcoinTestFramework):
 
         # Check node0's total balance; should be same as before the clone, + 100 BTC for 2 matured,
         # less possible orphaned matured subsidy
-        expected += 100
+        expected += 20000
         if (self.options.mine_block): 
             expected -= 50
         assert_equal(self.nodes[0].getbalance(), expected)
@@ -149,7 +149,7 @@ class TxnMallTest(BitcoinTestFramework):
                                                                 + fund_foo_tx["fee"]
                                                                 -   29
                                                                 + fund_bar_tx["fee"]
-                                                                +  100)
+                                                                +  20000)
 
         # Node1's "from0" account balance
         assert_equal(self.nodes[1].getbalance("from0", 0), -(tx1["amount"] + tx2["amount"]))
