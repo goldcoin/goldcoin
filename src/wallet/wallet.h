0eb7910a21f7d802c20e1227672210eccf0d7c2c
Stouse49 <Stouse49@gmail.com>
2018-01-01 22:04:32 -0800

    Fees (#15):  Default fees set to 0.001 GLD.  Since this was the same as Litecoin 0.14.2, returned other values to Litecoin values, except for fall back fee.

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -49,11 +49,11 @@ static const unsigned int DEFAULT_KEYPOOL_SIZE = 100;
 //! -paytxfee default
 static const CAmount DEFAULT_TRANSACTION_FEE = 0;
 //! -fallbackfee default
-static const CAmount DEFAULT_FALLBACK_FEE = 2000000;
+static const CAmount DEFAULT_FALLBACK_FEE = 100000;
 //! -mintxfee default
 static const CAmount DEFAULT_TRANSACTION_MINFEE = 100000;
 //! minimum recommended increment for BIP 125 replacement txs
-static const CAmount WALLET_INCREMENTAL_RELAY_FEE = 100000;
+static const CAmount WALLET_INCREMENTAL_RELAY_FEE = 5000;
 //! target minimum change amount
 static const CAmount MIN_CHANGE = CENT;
 //! final minimum change amount after paying for fees
