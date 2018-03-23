60ae3158c95b1523aa102ccb0b21a5bda8b54975
Stouse49 <Stouse49@gmail.com>
2018-03-22 22:23:21 -0700

    Update man page generator

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -4,23 +4,23 @@ TOPDIR=${TOPDIR:-$(git rev-parse --show-toplevel)}
 SRCDIR=${SRCDIR:-$TOPDIR/src}
 MANDIR=${MANDIR:-$TOPDIR/doc/man}
 
-BITCOIND=${BITCOIND:-$SRCDIR/goldcoind}
-BITCOINCLI=${BITCOINCLI:-$SRCDIR/goldcoin-cli}
-BITCOINTX=${BITCOINTX:-$SRCDIR/goldcoin-tx}
-BITCOINQT=${BITCOINQT:-$SRCDIR/qt/goldcoin-qt}
+GOLDCOIND=${GOLDCOIND:-$SRCDIR/goldcoind}
+GOLDCOINCLI=${GOLDCOINCLI:-$SRCDIR/goldcoin-cli}
+GOLDCOINTX=${GOLDCOINTX:-$SRCDIR/goldcoin-tx}
+GOLDCOINQT=${GOLDCOINQT:-$SRCDIR/qt/goldcoin-qt}
 
-[ ! -x $BITCOIND ] && echo "$GOLDCOIND not found or not executable." && exit 1
+[ ! -x $GOLDCOIND ] && echo "$GOLDCOIND not found or not executable." && exit 1
 
 # The autodetected version git tag can screw up manpage output a little bit
 BTCVER=($($GOLDCOINCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
 
 # Create a footer file with copyright content.
-# This gets autodetected fine for bitcoind if --version-string is not set,
-# but has different outcomes for bitcoin-qt and bitcoin-cli.
+# This gets autodetected fine for goldcoind if --version-string is not set,
+# but has different outcomes for goldcoin-qt and goldcoin-cli.
 echo "[COPYRIGHT]" > footer.h2m
-$BITCOIND --version | sed -n '1!p' >> footer.h2m
+$GOLDCOIND --version | sed -n '1!p' >> footer.h2m
 
-for cmd in $BITCOIND $BITCOINCLI $BITCOINTX $BITCOINQT; do
+for cmd in $GOLDCOIND $GOLDCOINCLI $GOLDCOINTX $GOLDCOINQT; do
   cmdname="${cmd##*/}"
   help2man -N --version-string=${BTCVER[0]} --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
   sed -i "s/\\\-${BTCVER[1]}//g" ${MANDIR}/${cmdname}.1
