bb965b397b4ae94ee28a0b402856579715409376
Stouse49 <Stouse49@gmail.com>
2018-03-23 21:30:45 -0700

    Update copyright years for GoldCoin (since 2013) and rename Goldcoin to GoldCoin

--- src/bitcoin-cli.cpp
+++ src/bitcoin-cli.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) 2007-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2015 The Bitcoin Core developers
 // Copyright (c) 2011-2017 The Litecoin Core developers
-// Copyright (c) 2013-2017 The Goldcoin Core developers
+// Copyright (c) 2013-2018 The GoldCoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
