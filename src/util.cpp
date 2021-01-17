010bf3b6f922c0a5c92837d2bd590f9a09d4b906
Crypto Canary <ssaxton@kent.edu>
2021-01-16 21:07:32 -0500

    Change default data directory from /GoldCoin (GLD) to /Goldcoin (#37)
    Change default data dir for Windows and Mac

--- src/util.cpp
+++ src/util.cpp
@@ -485,13 +485,13 @@ void PrintExceptionContinue(const std::exception* pex, const char* pszThread)
 boost::filesystem::path GetDefaultDataDir()
 {
     namespace fs = boost::filesystem;
-    // Windows < Vista: C:\Documents and Settings\Username\Application Data\GoldCoin (GLD)
-    // Windows >= Vista: C:\Users\Username\AppData\Roaming\GoldCoin (GLD)
-    // Mac: ~/Library/Application Support/GoldCoin (GLD)
+    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Goldcoin
+    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Goldcoin
+    // Mac: ~/Library/Application Support/Goldcoin
     // Unix: ~/.goldcoin
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "GoldCoin (GLD)";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "Goldcoin";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -501,7 +501,7 @@ boost::filesystem::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/GoldCoin (GLD)";
+    return pathRet / "Library/Application Support/Goldcoin";
 #else
     // Unix
     return pathRet / ".goldcoin";
