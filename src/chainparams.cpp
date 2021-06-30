5b591d070b344e5471632d4a041ab13258c9db97
Stouse49 <Stouse49@gmail.com>
2021-06-30 08:14:12 -0700

    remove obsolete dns seeders

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -129,8 +129,6 @@ public:
         // Note that of those with the service bits flag, most only support a subset of possible options
         vSeeds.push_back(CDNSSeedData("dnsseed.netseed.net", "dnsseed.netseed.net", true));
         vSeeds.push_back(CDNSSeedData("seed.glcseed.net", "seed.glcseed.net", true));
-        vSeeds.push_back(CDNSSeedData("dnsseed.gldcoin.com", "dnsseed.gldcoin.com", true));
-        vSeeds.push_back(CDNSSeedData("dnsseed.goldcoin.org", "dnsseed.goldcoin.org", true));
         vSeeds.push_back(CDNSSeedData("vps.netseed.net", "vps.netseed.net", true));
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,32);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
