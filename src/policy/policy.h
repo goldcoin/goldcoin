d42ebf487638372014143d542f3d20f3d064ddfb
MicroGuy <webmaster@microguy.net>
2022-11-04 16:10:53 -0400

    Defeat GIP1 Checks and Force 32 MB Global Limit

--- src/policy/policy.h
+++ src/policy/policy.h
@@ -1,7 +1,7 @@
 // Copyright (c) 2007-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2015 The Bitcoin Core developers
 // Copyright (c) 2011-2017 The Litecoin Core developers
-// Copyright (c) 2013-2018 The Goldcoin Core developers
+// Copyright (c) 2013-2023 The Goldcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -18,9 +18,12 @@
 class CCoinsViewCache;
 
 /** Default for -blockmaxsize, which controls the maximum size of block the mining code will create **/
-static const unsigned int DEFAULT_BLOCK_MAX_SIZE = 2000000;
-/** Default for -blockmaxsize, which controls the maximum size of block the mining code will create **/
+static const unsigned int DEFAULT_BLOCK_MAX_SIZE = 32000000;
+
+// GIP1 -blockmaxsize left to preserve functionality during sync testing
+// If successfull, we can consider reverting the GIP1 commits introducing this variable
 static const unsigned int DEFAULT_BLOCK_MAX_SIZE_GIP1 = 32000000;
+
 /** Default for -blockprioritysize, maximum space for zero/low-fee transactions **/
 static const unsigned int DEFAULT_BLOCK_PRIORITY_SIZE = DEFAULT_BLOCK_MAX_SIZE * 5 / 100; //5% are allowed to be free
 /** Default for -blockprioritysize, maximum space for zero/low-fee transactions **/
