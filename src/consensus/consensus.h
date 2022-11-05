f1adbd053bf2c6795ebbeef3966581c53782e148
MicroGuy <webmaster@microguy.net>
2022-11-04 22:25:15 -0400

    Increase MAX_BLOCK_SIGOPS_COST * 16

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -18,7 +18,7 @@ static const unsigned int MAX_BLOCK_SERIALIZED_SIZE = 32000000;
 /** The maximum allowed size for a block, in bytes (network rule) */
 static const unsigned int MAX_BLOCK_BASE_SIZE = 32000000;
 /** The maximum allowed number of signature check operations in a block (network rule) */
-static const int64_t MAX_BLOCK_SIGOPS_COST = 40000;
+static const int64_t MAX_BLOCK_SIGOPS_COST = 40000 * 16;
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
 
 static const int COINBASE_MATURITY = 100;
