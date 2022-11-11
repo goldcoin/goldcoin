2da01947cadd68131678518e1181e27ed8a532c3
MicroGuy <webmaster@microguy.net>
2022-11-11 18:06:40 -0500

    Remove redundant explicitly defined copy constructors

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -47,11 +47,6 @@ CTxMemPoolEntry::CTxMemPoolEntry(const CTransactionRef& _tx, const CAmount& _nFe
     nSigOpCostWithAncestors = sigOpCost;
 }
 
-CTxMemPoolEntry::CTxMemPoolEntry(const CTxMemPoolEntry& other)
-{
-    *this = other;
-}
-
 double
 CTxMemPoolEntry::GetPriority(unsigned int currentHeight) const
 {
