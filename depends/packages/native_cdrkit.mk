37236e8c742b04dea6de7f337f7f3174b213dbc0
koraycoding <kbicer@proton.me>
2022-11-11 18:23:53 +0000

    bulk update & patches

--- depends/packages/native_cdrkit.mk
+++ depends/packages/native_cdrkit.mk
@@ -1,6 +1,6 @@
 package=native_cdrkit
 $(package)_version=1.1.11
-$(package)_download_path=http://distro.ibiblio.org/fatdog/source/600/c
+$(package)_download_path=https://distro.ibiblio.org/fatdog/source/600/c
 $(package)_file_name=cdrkit-$($(package)_version).tar.bz2
 $(package)_sha256_hash=b50d64c214a65b1a79afe3a964c691931a4233e2ba605d793eb85d0ac3652564
 $(package)_patches=cdrkit-deterministic.patch
@@ -9,16 +9,22 @@ define $(package)_preprocess_cmds
   patch -p1 < $($(package)_patch_dir)/cdrkit-deterministic.patch
 endef
 
+# Starting with 10.1, GCC defaults to -fno-common, resulting in linking errors.
+# Pass -fcommon to retain the legacy behaviour.
 define $(package)_config_cmds
-  cmake -DCMAKE_INSTALL_PREFIX=$(build_prefix)
+  cmake -GNinja -DCMAKE_INSTALL_PREFIX=$(build_prefix) -DCMAKE_C_FLAGS="-fcommon"
 endef
 
 define $(package)_build_cmds
   $(MAKE) genisoimage
 endef
 
+# Older versions of cmake do not generate install target properly, but we
+# need to support them because that's what is in xenial and we use xenial
+# for reproducible builds. So we just fallback on installing everything.
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) -C genisoimage install
+  DESTDIR=$($(package)_staging_dir) ninja genisoimage/install || \
+  DESTDIR=$($(package)_staging_dir) ninja install
 endef
 
 define $(package)_postprocess_cmds
