e4c71bcec99750c0c75d20722f7a90a6ba171545
MicroGuy <webmaster@microguy.net>
2022-11-14 18:29:26 -0500

    Update guiutil.h

--- src/qt/guiutil.h
+++ src/qt/guiutil.h
@@ -241,6 +241,14 @@ namespace GUIUtil
 #else
     typedef ClickableProgressBar ProgressBar;
 #endif
+    
+    /**
+     * Returns the start-moment of the day in local time.
+     *
+     * QDateTime::QDateTime(const QDate& date) is deprecated since Qt 5.15.
+     * QDate::startOfDay() was introduced in Qt 5.14.
+     */
+    QDateTime StartOfDay(const QDate& date);    
 
 } // namespace GUIUtil
 
