458272fef3d8fe140a8ff6ad542508ca1a29e0ce
microguy <webmaster@microguy.net>
2025-08-01 14:51:01 +0000

    build: Remove fix_qt_pkgconfig patch for Qt 5.15.12
    The fix_qt_pkgconfig patch was designed for Qt 5.7.1 and is incompatible
    with Qt 5.15.12's qt_module.prf structure. Qt 5.15.12 should handle
    pkg-config generation correctly without this patch.

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -8,7 +8,7 @@ $(package)_dependencies=openssl zlib
 $(package)_linux_dependencies=freetype fontconfig libxcb libX11 xproto libXext
 $(package)_build_subdir=qtbase
 $(package)_qt_libs=corelib network widgets gui plugins testlib
-$(package)_patches=mac-qmake.conf fix_qt_pkgconfig.patch
+$(package)_patches=mac-qmake.conf
 $(package)_patches_linux=fix-xcb-include-order.patch
 
 $(package)_qttranslations_file_name=qttranslations-$($(package)_suffix)
@@ -134,7 +134,6 @@ define $(package)_preprocess_cmds
   cp -f qtbase/mkspecs/macx-clang/Info.plist.app qtbase/mkspecs/macx-clang-linux/ &&\
   cp -f qtbase/mkspecs/macx-clang/qplatformdefs.h qtbase/mkspecs/macx-clang-linux/ &&\
   cp -f $($(package)_patch_dir)/mac-qmake.conf qtbase/mkspecs/macx-clang-linux/qmake.conf && \
-  patch -p1 < $($(package)_patch_dir)/fix_qt_pkgconfig.patch && \
   echo "!host_build: QMAKE_CFLAGS     += $($(package)_cflags) $($(package)_cppflags)" >> qtbase/mkspecs/common/gcc-base.conf && \
   echo "!host_build: QMAKE_CXXFLAGS   += $($(package)_cxxflags) $($(package)_cppflags)" >> qtbase/mkspecs/common/gcc-base.conf && \
   echo "!host_build: QMAKE_LFLAGS     += $($(package)_ldflags)" >> qtbase/mkspecs/common/gcc-base.conf && \
