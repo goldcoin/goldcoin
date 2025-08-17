d8fdb3b07edbfda8eed877d9a78d9aa1a9141ab2
Goldcoin Senior Developer <goldcoin@goldcoinproject.org>
2025-08-17 15:15:32 -0700

    feat: Final cleanup - RPC, tests, and stragglers
    RPC directory:
    - mining.cpp: 1 instance eliminated
    - Other RPC files: sed cleanup applied
    
    Test files:
    - Eliminated all BOOST_FOREACH from test directory
    - 13+ instances modernized
    
    Stragglers:
    - httprpc.cpp: 1 instance + removed boost/foreach.hpp include
    - txmempool.cpp: 1 remaining instance eliminated
    
    All instances now use C++11 range-based for loops

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -71,7 +71,7 @@ void RPCTypeCheck(const UniValue& params,
                   bool fAllowNull)
 {
     unsigned int i = 0;
-    BOOST_FOREACH(UniValue::VType t, typesExpected)
+    for (UniValue::VType t : typesExpected)
     {
         if (params.size() <= i)
             break;
@@ -110,7 +110,7 @@ void RPCTypeCheckObj(const UniValue& o,
 
     if (fStrict)
     {
-        BOOST_FOREACH(const string& k, o.getKeys())
+        for (const string& k : o.getKeys())
         {
             if (typesExpected.count(k) == 0)
             {
@@ -189,7 +189,7 @@ std::string CRPCTable::help(const std::string& strCommand) const
         vCommands.push_back(make_pair(mi->second->category + mi->first, mi->second));
     sort(vCommands.begin(), vCommands.end());
 
-    BOOST_FOREACH(const PAIRTYPE(string, const CRPCCommand*)& command, vCommands)
+    for (const PAIRTYPE(string : const CRPCCommand*)& command, vCommands)
     {
         const CRPCCommand *pcmd = command.second;
         string strMethod = pcmd->name;
