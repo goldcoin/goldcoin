b1031253bca60394390e104b60260a7f70e9fcb0
Goldcoin Senior Developer <goldcoin@goldcoinproject.org>
2025-08-18 06:49:51 -0700

    Fix smart pointer conversion issues
    - Fix unique_ptr assertion in util.cpp
    - Fix net_processing.cpp pfilter conversions to use make_unique
    - Fix httpserver.cpp thread creation to use .get() on unique_ptr
    
    Note: goldcoind crashes during static initialization - debugging needed

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -2564,8 +2564,7 @@ bool static ProcessMessage(CNode* pfrom, const std::string& strCommand, CDataStr
         else
         {
             LOCK(pfrom->cs_filter);
-            delete pfrom->pfilter;
-            pfrom->pfilter = new CBloomFilter(filter);
+            pfrom->pfilter = std::make_unique<CBloomFilter>(filter);
             pfrom->pfilter->UpdateEmptyFull();
             pfrom->fRelayTxes = true;
         }
@@ -2601,8 +2600,7 @@ bool static ProcessMessage(CNode* pfrom, const std::string& strCommand, CDataStr
     {
         LOCK(pfrom->cs_filter);
         if (pfrom->GetLocalServices() & NODE_BLOOM) {
-            delete pfrom->pfilter;
-            pfrom->pfilter = new CBloomFilter();
+            pfrom->pfilter = std::make_unique<CBloomFilter>();
         }
         pfrom->fRelayTxes = true;
     }
