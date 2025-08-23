5db4c15a43326a980b7ed4a43dce694111d2993d
Goldcoin Senior Developer <goldcoin@goldcoinproject.org>
2025-08-23 09:11:14 -0700

    fix: Update version_info.h to 0.17.0 to match bitcoin-config.h
    Eliminates compiler warnings about PACKAGE_VERSION redefinition.
    Cherry-picked from commit 2204abe3c.

--- src/version_info.h
+++ src/version_info.h
@@ -8,10 +8,10 @@
 // Project identification
 #define PACKAGE_NAME "Goldcoin Core"
 #define PACKAGE_TARNAME "goldcoin"
-#define PACKAGE_VERSION "0.14.2"
-#define PACKAGE_STRING "Goldcoin Core 0.14.2"
+#define PACKAGE_VERSION "0.17.0"
+#define PACKAGE_STRING "Goldcoin Core 0.17.0"
 #define PACKAGE_BUGREPORT "https://github.com/goldcoin/goldcoin/issues"
-#define PACKAGE_URL "https://goldcoin.org/"
+#define PACKAGE_URL "https://www.goldcoinproject.org"
 
 // Copyright information
 #define COPYRIGHT_YEAR 2025
