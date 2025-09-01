cc043ede5ccbd398dbd79b3acc647722f8e4243f
Goldcoin Senior Developer <goldcoin@goldcoinproject.org>
2025-09-01 07:34:56 -0700

    Historic: First crypto to create native BDB 18.1 wallets
    Update FEATURE_LATEST to 170000 (BDB 18.1 native support).
    All new wallets now created with proper version alignment.
    Goldcoin achieves what Bitcoin Core couldn't - seamless BDB evolution.

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -90,7 +90,8 @@ enum WalletFeature
     FEATURE_COMPRPUBKEY = 60000, // compressed public keys
 
     FEATURE_HD = 130000, // Hierarchical key derivation after BIP32 (HD Wallet)
-    FEATURE_LATEST = FEATURE_COMPRPUBKEY // HD is optional, use FEATURE_COMPRPUBKEY as latest version
+    FEATURE_BDB18 = 170000, // Berkeley DB 18.1 native support (first crypto to achieve BDB 4.8→18.1 migration)
+    FEATURE_LATEST = FEATURE_BDB18 // Updated for v0.17.0 - all new wallets use BDB 18.1 format
 };
 
 
